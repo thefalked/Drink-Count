7eee36ae6d8956b6bcaf04be0778c006f787f5af
Giuliano Crivelli <giuliano@agenciaw3.digital>
2021-07-06 16:04:45 -0400

    starting to build the drink card

--- src/components/Drink.tsx
+++ src/components/Drink.tsx
@@ -1,28 +1,50 @@
 import {
   Flex,
-  Heading,
-  Input,
-  Button,
+  Image,
+  Text,
   useColorModeValue,
+  useMediaQuery,
 } from "@chakra-ui/react";
 
 export function Drink() {
-  const formBackground = useColorModeValue("gray.100", "gray.700");
+  const formBackground = useColorModeValue("teal.400", "gray.700");
+  const [isBiggerThan425] = useMediaQuery("(min-width: 425px)");
+
+  const liters = "2.0L";
+  const totalPrice = "R$ 1.121,00";
+
+  const drinks = {
+    heineken: "heineken.png",
+    budweise: "budweise.png",
+    default: "default.png",
+  };
 
   return (
-    <Flex mt="5" align="center" justifyContent="center">
-      <Flex direction="column" background={formBackground} p={12} rounded={6}>
-        <Heading mb={6}>Log in</Heading>
-        <Input
-          type="email"
-          placeholder="youremail@email.com"
-          variant="filled"
-          mb={3}
+    <Flex align="center" justifyContent="center">
+      <Flex
+        direction="column"
+        alignItems="center"
+        background={formBackground}
+        p={6}
+        rounded={6}
+        w="100%"
+      >
+        <Image
+          alt="Heineken"
+          src={`/logos/${drinks["heineken"]}`}
+          borderRadius="full"
+          boxSize="96px"
+          background="gray.100"
+          color="teal.400"
+          mb={4}
+          objectFit="contain"
         />
-        <Input type="password" placeholder="********" variant="filled" mb={3} />
-        <Button mb={6} colorScheme="teal">
-          Log in
-        </Button>
+        <Text color="gray.50" display="block" mb={2}>
+          Litros: {liters}
+        </Text>
+        <Text color="gray.50" display="block" textAlign="center">
+          Valor{isBiggerThan425 ? ":" : <br />} {totalPrice}
+        </Text>
       </Flex>
     </Flex>
   );
