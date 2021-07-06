213430e699848060249f6fc54bb8a01610b9047f
Giuliano Crivelli <giuliano@agenciaw3.digital>
2021-07-06 16:05:11 -0400

    adding more drinks and the bottom navigator

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
+import { Container, SimpleGrid } from "@chakra-ui/react";
 
 import { Header } from "../components/Header";
 import { Drink } from "../components/Drink";
+import { BottomNavigator } from "../components/BottomNavigator";
 
 export default function Home() {
   return (
@@ -12,7 +14,19 @@ export default function Home() {
 
       <Header />
 
-      <Drink />
+      <Container
+        maxW={["container.sm", "container.md", "container.lg", "container.xl"]}
+        mt={4}
+      >
+        <SimpleGrid spacing={4} columns={{ base: 1, sm: 2, md: 3, lg: 4 }}>
+          <Drink />
+          <Drink />
+          <Drink />
+          <Drink />
+        </SimpleGrid>
+      </Container>
+
+      <BottomNavigator />
     </>
   );
 }
