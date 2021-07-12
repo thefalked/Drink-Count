2755741342e71152d720403dbbee657a8c310115
Giuliano Crivelli <giuliano@agenciaw3.digital>
2021-07-12 15:31:11 -0400

    created money input with mask has a component

---
+++ src/components/InputMoneyMask.tsx
@@ -0,0 +1,36 @@
+import { ChangeEvent } from "react";
+import { Input, InputProps } from "@chakra-ui/react";
+
+import { stringSplice } from "../utils";
+import { useLocale } from "../hooks/useLocale";
+
+type InputMoneyMaskProps = InputProps & {
+  moneyInput: number;
+  setMoneyInput: (money: number) => void;
+};
+
+export function InputMoneyMask({
+  moneyInput,
+  setMoneyInput,
+  ...rest
+}: InputMoneyMaskProps) {
+  const { formatMoney } = useLocale();
+
+  function removeFormat(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target as HTMLInputElement;
+
+    const inputValueUnformatted = value.replace(/[^0-9]/g, "");
+
+    const valueWithDot = stringSplice(inputValueUnformatted, -2, ".");
+
+    setMoneyInput(Number(valueWithDot));
+  }
+
+  return (
+    <Input
+      onChange={event => removeFormat(event)}
+      value={formatMoney(moneyInput)}
+      {...rest}
+    />
+  );
+}
