5aee27c2e1b187cade004aeb8e4845f09311c078
thefalked <giulianocrivelli@hotmail.com>
2021-07-13 17:09:21 -0400

    fixing og:image name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap"
             rel="stylesheet"
           />
-          <meta property="og:image" content="/favicon.png"></meta>
+          <meta property="og:image" content="/logo.png"></meta>
 
           <link rel="shortcut icon" href="/favicon.ico" type="image/ico" />
         </Head>
