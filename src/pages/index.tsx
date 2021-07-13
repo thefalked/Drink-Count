e4a6539543dcebdc87cd82231e242f55871658bb
Giuliano Crivelli <giuliano@agenciaw3.digital>
2021-07-13 10:43:32 -0400

    adding quantity to calculations on drinksFormatted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
 import Head from "next/head";
 import { Container, SimpleGrid } from "@chakra-ui/react";
 
-import { Drink } from "../components/Drink";
-
 import { useDrink } from "../hooks/useDrink";
 import { useLocale } from "../hooks/useLocale";
+
+import { Drink } from "../components/Drink";
 
 export default function Home() {
   const { drinks } = useDrink();
   const { formatMoney } = useLocale();
 
-  const drinksFormatted = drinks.map(({ id, name, price, size }) => {
+  const drinksFormatted = drinks.map(({ id, name, price, quantity, size }) => {
     return {
       id,
       name,
-      price: formatMoney(price),
-      size: size.toFixed(2),
+      price: formatMoney(price * quantity),
+      size: ((size / 1000) * quantity).toFixed(2),
     };
   });
 
