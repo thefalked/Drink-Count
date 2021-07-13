02588e85928c2d6cdba4ced091e8a3b1c2b5ec91
Giuliano Crivelli <giuliano@agenciaw3.digital>
2021-07-13 16:06:40 -0400

    added config to vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,9 @@
+const { initReactI18next } = require("react-i18next");
+
 module.exports = {
   i18n: {
     locales: ["en", "pt-BR"],
     defaultLocale: "en",
   },
+  use: [initReactI18next],
 };
