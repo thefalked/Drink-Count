1325b15cbcb335a09f316b2ceda2d8f3616c7462
thefalked <giulianocrivelli@hotmail.com>
2021-11-10 14:45:23 -0400

    updated project to next 12 and some other packages

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -4,6 +4,6 @@ module.exports = {
   i18n: {
     locales: ["en", "pt-BR"],
     defaultLocale: "en",
-    localePath: path.resolve("./public/locales")
+    localePath: path.resolve("./public/locales"),
   },
 };
