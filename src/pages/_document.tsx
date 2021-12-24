49e7940c4665cc432609df6c06d4fdedbc1f3aeb
thefalked <giulianocrivelli@hotmail.com>
2021-12-24 17:48:47 -0400

    refactor: _document page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import { ColorModeScript } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 
-export default class MyDocument extends Document {
+export default class Document extends NextDocument {
   render() {
     return (
       <Html>
