f1a33a4dcdc389379ac408cdd7fa97619bab9ce5
thefalked <giulianocrivelli@hotmail.com>
2021-12-25 17:02:50 -0400

    feat: check if checkbox is not workin only on card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Container, SimpleGrid } from "@chakra-ui/react";
+import { Checkbox, Container, SimpleGrid } from "@chakra-ui/react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import type { GetServerSidePropsContext } from "next";
@@ -69,6 +69,7 @@ export default function Home({ host, locale }: HomeProps) {
             <Drink key={drink.id} drink={drink} />
           ))}
         </SimpleGrid>
+        <Checkbox></Checkbox>
       </Container>
     </>
   );
